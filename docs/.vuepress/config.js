1915684a78fcd8295f7eaf7cf6ecc5b045ffacce
zhangwei <1903404743@qq.com>
2021-06-24 11:38:28 +0800

    img path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -66,7 +66,7 @@ module.exports = {
     editLinkText: '编辑此页',
     // 导航
     nav: [
-      { text: '小打卡工作沉淀',
+      { text: '工作沉淀',
         items: [
           { text: '推荐系统', link: '/小打卡工作沉淀/后端/系统介绍/推荐系统.md'}
           ]
